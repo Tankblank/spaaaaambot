bb279a2adddbc754e2c2c65bb0c7b83650c8c0a9
Tankblank <40700065+Tankblank@users.noreply.github.com>
2018-10-21 01:39:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var ServerID = "501681063622606849"; //اي دي السيرفر
 var ChannelID = "503346874288308225";// اي دي الروم
 
 
+
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -17,59 +19,13 @@ client.on('disconnect', () => console.log('PROBOT credits miner had disconnected
 
 client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
 
-Client.on("guildMemberAdd", member => {
-   member.guild.defaultChannel.send("Welcome to: " + member.guild.name + " Hope you enjoy it here")
-});
-
-Client.on("guildMemberRemove", member => {
-   member.guild.defaultChannel.send("Goodbye: " + member.user.username + " from " + member.guild.name)
-});
-
-Client.on("guildCreate", guild => {
-	console.log("Some one added the test bot to a server created by: " + guild.owner.user.username)
-});
-
-Client.on("message", async (message) => {
-	if (message.author.bot) return;
-	if (!message.content.startsWith(prefix)) return;
-	
-	let command = message.content.split(" ")[0];
-	command = command.slice(prefix.length);
-	
-	let args = message.content.split(" ").slice(1);
-	
-	if (command === "ping") {
-		message.channel.send(`Pong! Time took: ${Date.now() - message.createdTimestamp} ms`);
-	} else
-
-	if (command === "say") {
-		message.delete()
-        const embed = new Discord.RichEmbed()
-		.setColor(0x954D23)
-		.setDescription(message.author.username + " says: " + args.join(" "));
-		message.channel.send({embed})
-	} else
-
-	if (command == "help") {
-		const embed = new Discord.RichEmbed()
-		.setColor(0x954D23)
-		.setTitle("Command List:")
-		.addField("!help", "Will give the current command list")
-		.addField("!ping", "WIll show the ping time for the bot")
-		.addField("!say [text]", "Will make the bot say something")
-		.addField("!announcement [text]", "Will make the bot say an announcement and tag everyone")
-		.addField("!cat", "Will send a random cat image");
-		message.channel.send({embed})
-	}
-
 
 function timerFunc() {
     client.on('message', msg => {
         client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7))
 
 
-
-});
+    });
 }
 
 var timer = setTimeout(timerFunc, 1000);
